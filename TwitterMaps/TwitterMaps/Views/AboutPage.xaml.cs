1723647ec638434d4c0a67976edcb6dbfe65196b
Greg Aronson <garonson@gmu.edu>
2018-01-27 11:13:32 -0500

    please work

---
+++ TwitterMaps/TwitterMaps/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace TwitterMaps
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
